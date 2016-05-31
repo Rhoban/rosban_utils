05d28594a6f31796310f6ed8c440f68b17db4c78
medrimonia <medrimonia@gmail.com>
2016-05-31 13:29:17 +0900

    Adding read_map<string,T> to xml_tools

--- include/rosban_utils/xml_tools.h
+++ include/rosban_utils/xml_tools.h
@@ -5,6 +5,8 @@
 
 #include <tinyxml.h>
 
+#include <functional>
+#include <map>
 #include <ostream>
 #include <vector>
 
@@ -108,6 +110,43 @@ void try_read_vector(TiXmlNode * node, const std::string &key, std::vector<T> & 
   if (!values) return;
 
   res = read_vector<T>(node, key);
+}
+
+/// Read a map which has the following structure
+/// <map_name>
+///   <entry>
+///     <key>...</key>
+///     <value>...</value>
+///   </entry>
+///   ... (other entries)
+/// </map_name>
+template <typename T>
+std::map<std::string,T> read_map(TiXmlNode * node,
+                                 const std::string & map_name,
+                                 std::function<T(TiXmlNode*)> builder)
+{
+  // Check for NULL node
+  if(!node) throw XMLParsingError("Null node when trying to get a vector");
+  // Get the root node
+  TiXmlNode* values = node->FirstChild(map_name);
+  if (!values) throw XMLParsingError("Could not find node with label '" + map_name + "' in node: '"
+                                   + node->Value() + "'");
+  // For each entry node:
+  std::map<std::string, T> result;
+  for (TiXmlNode * entry_node = values->FirstChild(); entry_node != NULL;
+       entry_node = entry_node->NextSibling()) {
+    std::string key = read<std::string>(entry_node, "key");
+    TiXmlNode * value_node = entry_node->FirstChild("val");
+    if (!value_node) {
+      throw XMLParsingError("Error while reading entry, missing val in entry_node '"
+                            + std::string(entry_node->Value()) + "'");
+    }
+    if (result.count(key) > 0) {
+      throw XMLParsingError("Key '" + key + "' found more than once in '" + node->Value() + "'");
+    }
+    result[key] = builder(value_node);
+  }
+  return result;
 }
 
 ///@param stream
